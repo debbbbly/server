4e4c4726ab1d36af2ae3089da09a3e559f6f337e
Vova Prudnikov <vovkasmail@gmail.com>
2025-07-22 16:58:37 -0700

    Update application configuration and add test profile

--- src/test/kotlin/com/debbly/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/debbly/server/ServerApplicationTests.kt
@@ -1,33 +1,15 @@
 package com.debbly.server
 
-import com.google.protobuf.util.JsonFormat
-import io.livekit.server.RoomServiceClient
-import livekit.LivekitModels
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import retrofit2.Call
-import retrofit2.Response
-
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ServerApplicationTests {
 
     @Test
     fun contextLoads() {
-        val client = RoomServiceClient.createClient(
-            "https://debbly.duckdns.org",
-            "",
-            ""
-        )
 
-        val call: Call<LivekitModels.Room> = client.createRoom("room_name")
-        val response: Response<LivekitModels.Room> = call.execute()
-        val room: LivekitModels.Room? = response.body()
-
-
-
-        System.out.println(JsonFormat.printer().print(room))
     }
-
-
 }
