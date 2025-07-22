f20cf8942bc9808d13c87982959e4a3ece5f983b
Vova Prudnikov <vladimir.prudnikov@roofstock.com>
2025-07-21 22:54:59 -0700

    Add application configuration and initial database changelog

--- src/test/kotlin/com/debbly/server/ServerApplicationTests.kt
+++ src/test/kotlin/com/debbly/server/ServerApplicationTests.kt
@@ -1,13 +1,31 @@
 package com.debbly.server
 
+import com.google.protobuf.util.JsonFormat
+import io.livekit.server.RoomServiceClient
+import livekit.LivekitModels
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import retrofit2.Call
+import retrofit2.Response
+
 
 @SpringBootTest
 class ServerApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
+    @Test
+    fun contextLoads() {
+        val client = RoomServiceClient.createClient(
+            "https://debbly.duckdns.org",
+            "",
+            ""
+        )
+
+        val call: Call<LivekitModels.Room> = client.createRoom("room_name")
+        val response: Response<LivekitModels.Room> = call.execute()
+        val room: LivekitModels.Room? = response.body()
+
+        System.out.println(JsonFormat.printer().print(room))
+    }
+
 
 }
