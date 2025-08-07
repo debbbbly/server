054708309f2658fb4253840ccdbd255eb34efe90
Vova Prudnikov <vovkasmail@gmail.com>
2025-08-06 20:41:47 -0700

    --

--- src/main/kotlin/com/debbly/server/config/RedisConfig.kt
+++ src/main/kotlin/com/debbly/server/config/RedisConfig.kt
@@ -1,23 +1,27 @@
-
 package com.debbly.server.config
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.cache.annotation.EnableCaching
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
 import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair
 import java.time.Duration
 
 @Configuration
 @EnableCaching
-class RedisConfig {
+class RedisConfig(private val objectMapper: ObjectMapper) {
 
     @Bean
     fun cacheConfiguration(): RedisCacheConfiguration {
         return RedisCacheConfiguration.defaultCacheConfig()
             .entryTtl(Duration.ofMinutes(5))
             .disableCachingNullValues()
-            .serializeValuesWith(SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer()))
+            .serializeValuesWith(SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer(objectMapper)))
     }
+
 }
